c1aae04fce6f67ac87715859649f722c9f047184
Amarjeet <ak3078184@gmail.com>
2023-09-05 18:50:05 +0530

    payment integration successfully integrated

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const cors = require('cors')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Razorpay = require('razorpay');
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-
-
+const razorpay = new Razorpay({
+    key_id: 'rzp_test_9selXw6bGldk59',
+    key_secret: '6WHOQ7qarNiWiqBDhDEVKbBw'
+});
 
 let foodItemsCollection;
 let foodCategoryCollection;
@@ -17,29 +20,38 @@ const mongoDB = async () => {
         await mongoose.connect('mongodb://127.0.0.1:27017/login1', {
             useNewUrlParser: true,
             useUnifiedTopology: true
-        })
-        console.log('Connected to MongoDB')
+        });
+        console.log('Connected to MongoDB');
 
         foodItemsCollection = mongoose.connection.db.collection("foot_items");
-        const data = await foodItemsCollection.find({}).toArray()
-        // console.log('Data from food_items collection:', data)
+        const data = await foodItemsCollection.find({}).toArray();
 
         foodCategoryCollection = mongoose.connection.db.collection("food_category");
-        const data1 = await foodCategoryCollection.find({}).toArray()
-        // console.log('Data from food_category collection:', data1)
+        const data1 = await foodCategoryCollection.find({}).toArray();
 
         app.get('/foodData', (req, res) => {
-            res.send(data)
-        })
-
+            res.send(data);
+        });
+    } catch (err) {
+        console.log(err);
     }
-    catch (err) {
-        console.log(err)
+};
+
+mongoDB();
+
+app.post('/create-order', async (req, res) => {
+    const options = {
+        amount: req.body.amount,
+        currency: 'INR',
+        receipt: 'YOUR_RECEIPT_ID'
+    };
+
+    try {
+        const order = await razorpay.orders.create(options);
+        res.json(order);
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating order' });
     }
-}
+});
 
-mongoDB()
-
-
-
-app.listen(3001, () => console.log('App listening on port 3001'))
+app.listen(3001, () => console.log('App listening on port 3001'));
